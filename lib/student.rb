08d22057563aa52a8bc75c92cc4a018381d5c73a
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-11-14 04:27:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,9 +49,15 @@ class Student
   end 
   
   def self.students_below_12th_grade
-    sql = <<-SQL 
+     sql = <<-SQL 
+            SELECT * 
+            FROM students 
+            WHERE grade < 12
     SQL
+    DB[:conn].execute(sql).collect do|row|
+    self.new_from_db(row)
   end 
+    end 
   
   def save
     sql = <<-SQL
