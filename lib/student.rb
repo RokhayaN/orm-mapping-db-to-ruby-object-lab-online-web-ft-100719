441a4631bf0731ef2dff32368bbcb6d7bd39ec08
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-11-14 04:51:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,8 +50,10 @@ class Student
   
   def self.first_X_students_in_grade_10
   end 
-  def self 
+  
+  def self.all_students_in_grade_X 
   end 
+  
   def self.students_below_12th_grade
      sql = <<-SQL 
             SELECT * 
